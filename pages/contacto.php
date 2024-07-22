afcef67921d81e30627bee836be2801c766a3c44
Franco Aparicio <francomaty07@gmail.com>
2024-07-22 18:25:57 -0300

    cambios 22/7

--- pages/contacto.php
+++ pages/contacto.php
@@ -7,24 +7,28 @@ include_once("../components/header.php");
 
     <main>
         <div class="main-personalizadas_div">
-            <form class="main-personalizadas_form" id="formulario-aside">
+            <form class="main-personalizadas_form" id="formulario">
                 <h3 class="main-personalizadas_h3">CONTACTO </h3>
                 <p class="main-personalizadas_p">¿Tienes alguna pregunta sobre nuestros productos o necesitas ayuda con tu pedido? Estamos aquí para ayudarte. Ya sea que estés buscando más información sobre nuestras remeras, buzos, medias o accesorios, o si necesitas asistencia con tu compra, no dudes en ponerte en contacto con nosotros.</p>
                 <div>
-                    <label for="name">Nombre</label>
-                    <input id="name" type="text" placeholder="Nombre">
+                    <label for="nombre">Nombre</label>
+                    <span id="errorNombre" style="color: red;"></span>
+                    <input id="nombre" type="text" placeholder="Nombre" name="nombre">
                 </div>
                 <div>
-                    <label for="mail">Email</label>
-                    <input id="mail" type="email" placeholder="Email">
+                    <label for="email">Email</label>
+                    <span id="errorEmail" style="color: red;"></span>
+                    <input id="email" type="email" placeholder="Email" name="email">
                 </div>
                 <div>
-                    <label for="num">Numero Telefono</label>
-                    <input id="num" type="text" placeholder="Numero">
+                    <label for="telefono">Numero Telefono</label>
+                    <span id="errorTelefono" style="color: red;"></span>
+                    <input id="telefono" type="number" placeholder="Numero" name="telefono">
                 </div>
                 <div>
-                    <label for="text">Mensaje</label>
-                    <textarea name="" id="text" placeholder="Consultanos..."></textarea>
+                    <label for="mensaje">Mensaje</label>
+                    <span id="errorMensaje" style="color: red;"></span>
+                    <textarea id="mensaje" placeholder="Consultanos..." name="mensaje"></textarea>
                 </div>
                 <div>
                     <input type="submit" value="Enviar">
@@ -32,7 +36,7 @@ include_once("../components/header.php");
             </form>
         </div>
     </main>
-
+    <script src="../javascript/forms-contacto.js"></script>
     <?php
 
     include_once("../components/footer.php");
